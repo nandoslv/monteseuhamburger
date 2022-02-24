b816f15fb0521ae5d746182f26dbf089803c25ca
Fernando dos Santos Silva <fernando.silva@PRODEST.ES.GOV.BR.LOCAL>
2022-02-24 08:34:34 -0300

    Primeiro commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+    <footer id=footer>
+        <p>Monte seu Hamburger &copy; 2021</p>
+    </footer>
+</template>
+
+<script>
+export default{
+    name: "Footer"
+}
+</script>
+
+<style scoped>
+#footer{
+    min-height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #fcba03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    bottom: 0;
+    width: 100%;
+}
+</style>
